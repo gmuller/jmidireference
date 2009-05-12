036476b06f914923ad5995254cc5d975736ab0c5
gmuller <gmuller@5b262f4c-556f-486a-9dc1-8d742896e0aa>
2009-05-12 17:07:27 +0000

    Added Several new features:
    RWMidi has a syncConversion routine, allowing 24ppq to be upsampled to 96ppq
    
    Changed MidiReference TimeBase to reflect this (and make it capable of several different sync types)
    
    Added Save/Load Feature for GOLsequencer (Global and per sequencer)
    
    
    
    git-svn-id: http://svn.grantmuller.com/full/Java/MidiReference@121 5b262f4c-556f-486a-9dc1-8d742896e0aa

--- src/midiReference/TimeBase.java
+++ src/midiReference/TimeBase.java
@@ -5,115 +5,139 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Enumeration containing the number of pulses per rythm value (based on a 24PPQ MIDI sync clock) 
+ * Enumeration containing the number of pulses per rhythm value (based on a 24PPQ MIDI sync clock) 
  * @author gmuller
  *
  */
-public enum TimeBase24 {
-	
+public enum TimeBase {
+
 	NONE(0),
-	
+
+	SIXTY_FOURTH_NOTE(1.5),
 	/** 
 	 * 2 pulses per 32nd Triplet
 	 */
 	THIRTY_SECOND_TRIPLET(2),
-	
+
 	/**
 	 * 3 pulses per 32nd note
 	 */
 	THIRTY_SECOND(3),
-	
+
 	/**
 	 * 4 Pulses per 16 Triplet
 	 */
 	SIXTEENTH_TRIPLET(4),
-	
+
 	/**
 	 * 6 pulses per 16th note
 	 */
 	SIXTEENTH(6),
-	
+
 	/**
 	 * 8 pulses per 8th Triplet
 	 */
 	EIGHTH_TRIPLET(8),
-	
+
 	/**
 	 * 9 pulses per dotted 16th note
 	 */
 	DOTTED_SIXTEENTH(9),
-	
+
 	/**
 	 * 12 pulses per 8th note
 	 */
 	EIGHTH(12),
-	
+
 	/**
 	 * 16 Pulses per 1/4 Triplet
 	 */
 	QUARTER_TRIPLET(16),
-	
+
 	/**
 	 * 18 pulses per dotted 8th note 
 	 */
 	DOTTED_EIGHTH(18),
-	
+
 	/**
 	 * 24 pulses per 1/4 note
 	 */
 	QUARTER(24),
-	
+
 	/**
 	 * 32 pulses per 1/2 triplet
 	 */
 	HALF_TRIPLET(32),
-	
+
 	/**
 	 * 36 pulses per dotted 1/4 note
 	 */
 	DOTTED_QUARTER(36),
-	
+
 	/**
 	 * 48 pulses per 1/2 note
 	 */
 	HALF(48),
-	
+
 	/**
 	 * 64 pulses per Whole Triplet
 	 */
 	WHOLE_TRIPLET(64),
-	
+
 	/**
 	 * 72 Pulses per dotted 1/2 note
 	 */
 	DOTTED_HALF(72),
-	
+
 	/**
 	 * 96 pulses per whole note
 	 */
 	WHOLE(96);
-	
-	private static final Map<Integer,TimeBase24> lookup 
-    = new HashMap<Integer,TimeBase24>();
-	
+
+	private static final Map<Integer,TimeBase> lookup 
+	= new HashMap<Integer,TimeBase>();
+
 	static {
-        for(TimeBase24 s : EnumSet.allOf(TimeBase24.class))
-             lookup.put(s.getValue(), s);
-   }
-	
-	private int pulseValue;
-	
-	TimeBase24(int pulseValue){
+		for(TimeBase s : EnumSet.allOf(TimeBase.class))
+			lookup.put(s.getValue(), s);
+	}
+
+	private double pulseValue;
+	private static int baseValue = 24; 
+
+	TimeBase(double pulseValue){
 		this.pulseValue = pulseValue;
 	}
-	
+
 	/**
 	 * Gets the pulse count reference associated with the enumeration
 	 * @return int pulse count reference
 	 */
-	public int getValue() { return pulseValue; }
+	public int getValue() { 
+		return (int) pulseValue; 
+	}
 	
-	public static TimeBase24 get(Integer stepSize) { 
-        return lookup.get(stepSize); 
-   }
+	/**
+	 * Gets the converted pulse count reference associated with the enumeration
+	 * @return int converted pulse count reference
+	 */
+	public int getValue(int convertTo) {
+		return (int) (pulseValue * (convertTo/baseValue)); 
+	}
+
+	/**
+	 * Gets the enumeration represented by the pulse count
+	 * @return int converted pulse count reference
+	 */
+	public static TimeBase get(Integer stepSize) { 
+		return lookup.get(stepSize); 
+	}
+
+	/**
+	 * Gets the enumeration represented by the converted pulse count
+	 * @return int converted pulse count reference
+	 */
+	public static TimeBase get(Integer stepSize, int convertTo) {
+		return lookup.get(stepSize / (convertTo/baseValue)); 
+	}
 }
